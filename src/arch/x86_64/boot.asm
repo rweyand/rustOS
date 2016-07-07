f62ee1ec1bcfac8d0529ae00a575b47f78ebf599
Reinhard Weyand <reinhard.weyand@zalando.de>
2016-07-07 09:02:38 +0200

    added paging

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -30,10 +30,7 @@ error:
   mov byte  [0xb800a], al
   hlt
 
-check_cpuid:
-    ; Check if CPUID is supported by attempting to flip the ID bit (bit 21)
-    ; in the FLAGS register. If we can flip it, CPUID is available.
-    ; Copy FLAGS in to EAX via stack
+check_cpuid: ; copied from osDEV
     pushfd
     pop eax
       ; Copy to ECX as well for comparing later on
@@ -63,24 +60,56 @@ check_cpuid:
     mov al, "1"
     jmp error
 
-check_long_mode:
-      ; test if extended processor info in available
-      mov eax, 0x80000000    ; implicit argument for cpuid
-      cpuid                  ; get highest supported argument
-      cmp eax, 0x80000001    ; it needs to be at least 0x80000001
-      jb .no_long_mode       ; if it's less, the CPU is too old for long mode
+check_long_mode: ; copied from OSDev
+  ; test if extended processor info in available
+  mov eax, 0x80000000    ; implicit argument for cpuid
+  cpuid                  ; get highest supported argument
+  cmp eax, 0x80000001    ; it needs to be at least 0x80000001
+  jb .no_long_mode       ; if it's less, the CPU is too old for long mode
+  ; use extended info to test if long mode is available
+  mov eax, 0x80000001    ; argument for extended processor info
+  cpuid                  ; returns various feature bits in ecx and edx
+  test edx, 1 << 29      ; test if the LM-bit is set in the D-register
+  jz .no_long_mode       ; If it's not set, there is no long mode
+  ret
+.no_long_mode:
+  mov al, "2"
+  jmp error
 
-      ; use extended info to test if long mode is available
-      mov eax, 0x80000001    ; argument for extended processor info
-      cpuid                  ; returns various feature bits in ecx and edx
-      test edx, 1 << 29      ; test if the LM-bit is set in the D-register
-      jz .no_long_mode       ; If it's not set, there is no long mode
-      ret
-  .no_long_mode:
-      mov al, "2"
-      jmp error
+set_up_page_tables:
+  ; map first P4 entry to adress of P3 table
+  mov eax, p3_table
+  or eax, 0b11 ; set present and writable bits
+  mov [p4_table], eax
+
+  mov eax, p2_table
+  or eax, 0b11
+  mov [p3_table], eax
+
+  ;  map each P2 entry to a huge 2MiB page
+  mov ecx, 0         ; counter variable
+
+  .map_p2_table:
+  ;loop through 512 entries and map one huge page
+  mov eax, 0x200000  ; 2MiB
+  mul ecx            ; start address of i-th page
+  or eax, 0b10000011 ; present + writable + huge
+  mov [p2_table + ecx * 8], eax ; map i-th entry
+  inc ecx            ; increase counter
+  cmp ecx, 512       ; if counter == 512, the whole P2 table is mapped
+  jne .map_p2_table  ; else map the next entry
+ret
 
 section .bss
+align 4096
+p4_table:
+  resb 4096
+p3_table:
+  resb 4096
+p2_table:
+  resb 4096
+stack_bottom:
+  resb 64
 stack_bottom:
   resb 64
 stack_top:
